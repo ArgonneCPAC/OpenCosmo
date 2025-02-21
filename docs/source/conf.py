608868870e96c2bd015bd33ca35bbd69e5319ca6
Patrick Wells <patrick.wells.95@gmail.com>
2025-02-21 12:56:19 -0600

    Add theme to extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
