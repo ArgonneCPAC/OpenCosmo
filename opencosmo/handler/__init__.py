51690432aa46c4ba61e5d561794c403b084d41f7
Patrick Wells <patrick.wells.95@gmail.com>
2025-02-24 15:30:58 -0600

    Scaffolding for dataset

--- opencosmo/handler/__init__.py
+++ opencosmo/handler/__init__.py
@@ -1,7 +1,8 @@
-import hdf5plugin  # noqa
 # There is some weirdness that can cause things to break if this
 # import is not done prior to trying to read data
+import hdf5plugin  # noqa: F401 # type: ignore
 
 from .handler import OpenCosmoDataHandler
+from .im import InMemoryHandler
 
-__all__ = ["OpenCosmoDataHandler"]
+__all__ = ["OpenCosmoDataHandler", "InMemoryHandler"]
