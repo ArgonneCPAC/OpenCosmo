c5946d76b39dd6a2a4caa45d8fa14443c07059fb
Patrick Wells <patrick.wells.95@gmail.com>
2025-02-24 09:03:46 -0600

    Update to read_cosmology

--- opencosmo/header.py
+++ opencosmo/header.py
@@ -1,11 +1,28 @@
+from functools import cached_property
+
 import h5py
 
+from opencosmo import cosmology as cosmo
 from opencosmo import parameters
 from opencosmo.file import file_reader
 
 
+class OpenCosmoHeader:
+    def __init__(
+        self,
+        cosmology_pars: parameters.CosmologyParameters,
+        simulation_pars: parameters.SimulationParameters,
+    ):
+        self.__cosmology_pars = cosmology_pars
+        self.__simulation_pars = simulation_pars
+
+    @cached_property
+    def cosmology(self):
+        return cosmo.make_cosmology(self.__cosmology_pars)
+
+
 @file_reader
-def read_header(file: h5py.File) -> parameters.SimulationParameters:
+def read_header(file: h5py.File) -> OpenCosmoHeader:
     try:
         cosmology_paramters = parameters.read_header_attributes(
             file, "simulation/cosmology", parameters.CosmologyParameters
@@ -27,4 +44,4 @@ def read_header(file: h5py.File) -> parameters.SimulationParameters:
             "This file does not appear to have simulation information. "
             "Are you sure it is an OpenCosmo file?"
         )
-    return simulation_parameters
+    return OpenCosmoHeader(cosmology_paramters, simulation_parameters)
