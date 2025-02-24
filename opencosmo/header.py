d76e222b4a9717c172bf7f55849441168ceb4740
Patrick Wells <patrick.wells.95@gmail.com>
2025-02-24 09:54:16 -0600

    Reformat paramters in place

--- opencosmo/header.py
+++ opencosmo/header.py
@@ -12,9 +12,11 @@ class OpenCosmoHeader:
         self,
         cosmology_pars: parameters.CosmologyParameters,
         simulation_pars: parameters.SimulationParameters,
+        reformat_pars: parameters.ReformatParamters,
     ):
         self.__cosmology_pars = cosmology_pars
         self.__simulation_pars = simulation_pars
+        self.__reformat_pars = reformat_pars
 
     @cached_property
     def cosmology(self):
@@ -32,6 +34,13 @@ def read_header(file: h5py.File) -> OpenCosmoHeader:
             "This file does not appear to have cosmology information. "
             "Are you sure it is an OpenCosmo file?"
         )
+
+    try:
+        reformat_paramters = parameters.read_header_attributes(
+            file, "reformat_hacc/config", parameters.ReformatParamters
+        )
+    except KeyError:
+        raise KeyError("File hader is malformedAre you sure it is an OpenCosmo file?")
     try:
         simulation_parameters = parameters.read_header_attributes(
             file,
@@ -44,4 +53,6 @@ def read_header(file: h5py.File) -> OpenCosmoHeader:
             "This file does not appear to have simulation information. "
             "Are you sure it is an OpenCosmo file?"
         )
-    return OpenCosmoHeader(cosmology_paramters, simulation_parameters)
+    return OpenCosmoHeader(
+        cosmology_paramters, simulation_parameters, reformat_paramters
+    )
