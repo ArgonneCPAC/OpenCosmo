e6458a1ec79833e6af20b4e3a5df1ca88f3b0355
Patrick Wells <patrick.wells.95@gmail.com>
2025-02-24 15:39:07 -0600

    Initial documentation for read

--- docs/source/read.rst
+++ docs/source/read.rst
@@ -1,6 +1,7 @@
 Reading Data
 ============
 
+.. autofunction:: opencosmo.read
 .. autofunction:: opencosmo.read_cosmology
 .. autofunction:: opencosmo.read_header
 .. autofunction:: opencosmo.read_simulation_parameters
--- opencosmo/dataset/dataset.py
+++ opencosmo/dataset/dataset.py
@@ -9,6 +9,24 @@ from opencosmo.header import OpenCosmoHeader, read_header
 
 @file_reader
 def read(file: h5py.File) -> OpenCosmoDataset:
+    """
+    Read a dataset from a file into memory.
+
+    You should use this function if the data are small enough that having
+    a copy of it (or a few copies of it) in memory is not a problem. For
+    larger datasets, use opencosmo.open
+
+    Parameters
+    ----------
+    file : str or pathlib.Path
+        The path to the file to read.
+
+    Returns
+    -------
+    dataset : OpenCosmoDataset
+        The dataset read from the file.
+
+    """
     header = read_header(file)
     handler = InMemoryHandler(file)
     return OpenCosmoDataset(handler, header)
