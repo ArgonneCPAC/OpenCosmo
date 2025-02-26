787a4d50613c838654b24ec2586336e66e0af733
Patrick Wells <patrick.wells.95@gmail.com>
2025-02-26 16:33:58 -0600

    Move dataset out of submodule

--- opencosmo/dataset/transformation/transformation.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional, Protocol
-
-from astropy.table import Column  # type: ignore
-
-
-class ColumnTransformation(Protocol):
-    def __call__(self, column: Column) -> Optional[Column]: ...
