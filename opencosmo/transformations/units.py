833116e93a16364facad19d08ee79469c170c58e
Patrick Wells <patrick.wells.95@gmail.com>
2025-02-26 12:42:48 -0600

    Complete unit handling on columns

--- opencosmo/transformations/units.py
+++ opencosmo/transformations/units.py
@@ -1,45 +1,72 @@
+from typing import Optional
+
 import astropy.cosmology.units as cu
 import astropy.units as u
 from astropy.table import Column, Table
 
 
-def apply_units(table: Table) -> None:
+def apply_units_by_name(table: Table) -> None:
     """
     InPlaceTableTransformation that applies units to the columns.
-
-    where are monads when you need them :'(
     """
-    parsers = get_column_parsers()
     for colname in table.columns:
-        is_updated = False
-        new_column = table[colname]
-        for parser in parsers:
-            updated_column = parser(new_column)
-            if updated_column is not None:
-                new_column = updated_column
-                is_updated = True
-        if is_updated:
-            table[colname] = new_column
+        column = table[colname]
+        if column.unit is not None:
+            continue
+        unit = parse_column_name(colname)
+        if unit is not None:
+            column.unit = unit
 
 
-def get_column_parsers():
-    return [parse_velocities, parse_mass]
+def parse_column_name(column_name: str) -> Optional[u.Quantity | u.Unit]:
+    name_parts = column_name.split("_")
+    if len(name_parts) == 1:
+        return parse_single_name(name_parts[0])
+    else:
+        return parse_multipart_name(name_parts)
 
 
-def parse_velocities(column: Column):
-    VELOCITY_COLUMNS = ["VX", "VY", "VZ"]
-    name = column.name.upper()
-    if name[-2:] in VELOCITY_COLUMNS:
-        column.unit = u.km / u.s
-        return column
+def parse_single_name(name: str) -> Optional[u.Quantity | u.Unit]:
+    match name:
+        case "x" | "y" | "z" | "hh":
+            return u.Mpc / cu.littleh
+        case "phi" | "uu":
+            return (u.km / u.s) ** 2
+        case "mass" | "mbh":
+            return u.Msun / cu.littleh
+        case "rho":
+            return (u.Msun / u.kpc**3) * cu.littleh**2
+        case "bhr":
+            return u.Msun / u.yr
+        case "age":  # Fix needed:
+            return None
+        case _:
+            return None
 
 
-def parse_mass(column: Column):
-    column_name_parts = column.name.split("_")
-
-    def is_mass_part(part):
-        return part == "mass" or part[0] == "M"
-
-    if any(is_mass_part(part) for part in column_name_parts):
-        column.unit = u.Msun / cu.littleh
-        return column
+def parse_multipart_name(name: list[str]) -> Optional[u.Quantity | u.Unit]:
+    if "mass" in name or name[-1][0].upper() == "M":
+        return u.Msun / cu.littleh
+    if "radius" in name or name[-1][0] == "R" or name[-1].startswith("rad"):
+        return u.Mpc / cu.littleh
+    elif "vel" in name or name[-1] in ["vx", "vy", "vz"]:
+        return u.km / u.s
+    elif name[-1] in ["x", "y", "z"]:
+        return u.Mpc / cu.littleh
+    elif name[-1][0] == "Y":
+        return (u.Mpc / cu.littleh) ** 2
+    elif name[-1][0] == "T":
+        return u.Kelvin
+    elif name[-1][0] == "L":
+        return u.DexUnit(u.erg / u.s)
+    elif name[-1] == "entropy":
+        return u.kev / u.cm**2
+    elif name[-1] == "ne":
+        return u.cm**-3
+    elif name[-1][0] == "t":
+        return u.Gyr
+    elif name[-1] in ["sfr", "bhr"]:
+        return u.Msun / u.yr
+    elif name[-1] == "ke":
+        return (u.km / u.s) ** 2
+    return None
