0956099190f7a9191134396f5ad5c3361b7f6516
Patrick Wells <patrick.wells.95@gmail.com>
2025-02-26 16:39:34 -0600

    Fix lint errors

--- opencosmo/transformations/transformation.py
+++ opencosmo/transformations/transformation.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Optional, Protocol, TypeVar
+from typing import Optional, Protocol
 
 import numpy as np
 from astropy.table import Column, Table  # type: ignore
