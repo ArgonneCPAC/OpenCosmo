546c5efdf6428885e63e0f18485296cba2faa8c8
Patrick Wells <patrick.wells.95@gmail.com>
2025-03-03 16:22:31 -0600

    Littleh now a all-col transformation

--- opencosmo/handler/im.py
+++ opencosmo/handler/im.py
@@ -29,8 +29,16 @@ class InMemoryHandler:
         """ """
         table_transformations = transformations.get(t.TransformationType.TABLE, [])
         column_transformations = transformations.get(t.TransformationType.COLUMN, [])
+        all_column_transformations = transformations.get(
+            t.TransformationType.ALL_COLUMNS, []
+        )
+        print(all_column_transformations)
+        print(table_transformations)
         new_data = copy(self.__data)
         new_data = t.apply_column_transformations(new_data, column_transformations)
+        new_data = t.apply_all_columns_transformations(
+            new_data, all_column_transformations
+        )
         new_data = t.apply_table_transformations(new_data, table_transformations)
         return new_data
 
@@ -39,6 +47,12 @@ class InMemoryHandler:
         # Problem: Transformations can rename columns...
         table_transformations = transformations.get(t.TransformationType.TABLE, [])
         column_transformations = transformations.get(t.TransformationType.COLUMN, [])
+        all_column_transformations = transformations.get(
+            t.TransformationType.ALL_COLUMNS, []
+        )
         new_data = t.apply_column_transformations(new_data, column_transformations)
+        new_data = t.apply_all_columns_transformations(
+            new_data, all_column_transformations
+        )
         new_data = t.apply_table_transformations(new_data, table_transformations)
         return new_data
