365c2d3e95bba3e0c6830399d1028de0bd79d4c3
Patrick Wells <patrick.wells.95@gmail.com>
2025-03-11 13:54:35 -0500

    Update to how MPI is invoked in workflow

--- opencosmo/handler/__init__.py
+++ opencosmo/handler/__init__.py
@@ -5,6 +5,12 @@ import hdf5plugin  # type: ignore # noqa: F401
 from .handler import OpenCosmoDataHandler
 from .im import InMemoryHandler
 from .oom import OutOfMemoryHandler
-from .mpi import MPIHandler
 
-__all__ = ["OpenCosmoDataHandler", "InMemoryHandler", "OutOfMemoryHandler", "MPIHandler"]
+__all__ = ["OpenCosmoDataHandler", "InMemoryHandler", "OutOfMemoryHandler"]
+try:
+    import mpi4py  # noqa: F401
+    from .mpi import MPIHandler
+    __all__.append("MPIHandler")
+except ImportError:
+    pass
+
