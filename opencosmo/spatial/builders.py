2b01a58c8bb4086d01dc9e4e1f4953264816f91c
Patrick Wells <patrick.wells.95@gmail.com>
2025-05-16 14:48:23 -0500

    Add missing file

---
+++ opencosmo/spatial/builders.py
@@ -0,0 +1,11 @@
+from typing import cast
+
+from opencosmo.spatial.region import BoxRegion, BoxSize, Point3d
+
+
+def Box(center: Point3d, width: float | BoxSize):
+    if isinstance(width, float):
+        width = (width, width, width)
+
+    halfwidth = cast(BoxSize, tuple(float(w / 2) for w in width))
+    return BoxRegion(center, halfwidth)
