3db652669847fb83f45f322776e2ea4611009a63
William Hicks <whicks@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2025-05-20 22:53:20 +0000

    optionally compute xray luminosities and stuff with pyxsim when loading into yt

--- opencosmo/analysis/utils.py
+++ opencosmo/analysis/utils.py
@@ -1,12 +1,29 @@
 import numpy as np
 import yt  # type: ignore
-from yt.units import Msun, Mpc, h  # type: ignore
+from unyt import unyt_quantity # type: ignore
 from astropy.table import Table  # type: ignore
 from yt.data_objects.static_output import Dataset as YT_Dataset  # type: ignore
 from yt.visualization.plot_window import PlotWindow # type: ignore
 from re import sub
+from pyxsim import CIESourceModel # type: ignore
+from typing import Optional, Dict, Any
 
-def create_yt_dataset(data: Table) -> YT_Dataset:
+
+
+# ---- define some constants ---- #
+mp = unyt_quantity(1.67262192595e-24, 'g')
+kB = unyt_quantity(1.380649e-16, 'erg/K')
+solar_metallicity = 0.012899 # value used internally in HACC
+# ------------------------------- #
+
+
+def create_yt_dataset(
+    data: Table, 
+    compute_xray_fields: Optional[bool] = None,
+    return_source_model: Optional[bool] = None,
+    source_model_kwargs: Optional[Dict[str, Any]] = {}
+) -> YT_Dataset:
+
     data_dict = {}
     minx, maxx = np.inf, -np.inf
     miny, maxy = np.inf, -np.inf
@@ -56,10 +73,80 @@ def create_yt_dataset(data: Table) -> YT_Dataset:
 
     ds = yt.load_particles(
         data_dict,
-        length_unit=1.0 * Mpc,
-        mass_unit=1.0 * Msun,
+        length_unit="Mpc",
+        mass_unit="Msun",
         bbox=bbox
     )
+
+    # add derived fields
+
+    # compute a new MMW field (TODO: find better solution)
+    ds.add_field(
+        ("gas", "MMW"),
+        function=_mmw,
+        units="",
+        sampling_type="particle",
+    )
+
+    ds.add_field(
+        ("gas", "temperature"),
+        function=_temperature,
+        units="K",
+        sampling_type="particle"
+    )
+
+    ds.add_field(
+        ("gas", "number_density"),
+        function=_number_density,
+        units="cm**-3",
+        sampling_type="particle",
+        force_override=True
+    )
+
+    ds.add_field(
+        ("gas", "metallicity"),
+        function=_metallicity,
+        units="Zsun",
+        sampling_type="particle"
+    )
+
+    if compute_xray_fields:
+        # compute xray luminosities, emissivities, etc. using pyxsim.
+        # This calls CIESourceModel, which assumes ionization equilibrium.
+        # User can define custom parameters 
+
+        ds.add_field(
+            ("gas", "emission_measure"),
+            function=_emission_measure,
+            units="cm**-3",
+            sampling_type="particle"
+        )
+
+        #TODO: Make sure redshift is passed into this list
+        default_kwargs = {
+            "model": "apec",
+            "emin": 0.1,  # keV
+            "emax": 10.0,  # keV
+            "nbins": 1000,
+            "Zmet": ("gas","metallicity"), # Zsun
+            "temperature_field": ("gas", "temperature"),
+            "emission_measure_field": ("gas", "emission_measure")
+        }
+
+        if source_model_kwargs is None:
+            source_model_kwargs = {}
+        
+        # update with user-defined settings
+        source_model_kwargs = {**default_kwargs, **source_model_kwargs}
+
+        # define xray source model (NOTE: this will download a few fits files needed for the analysis)
+        source = CIESourceModel(**source_model_kwargs)
+
+        # populate yt dataset with xray fields
+        source.make_source_fields(ds, source_model_kwargs["emin"], source_model_kwargs["emax"])
+
+        if return_source_model:
+            return ds, source
 
     return ds
 
@@ -71,3 +158,35 @@ def profile_plot(*args, **kwargs) -> PlotWindow:
 
 def phase_plot(*args, **kwargs) -> PlotWindow:
     return yt.PhasePlot(*args, **kwargs)
+
+
+
+# ---------- DERIVED FIELDS -------------- #
+
+def _mmw(field, data):
+    # Recompute mean molecular weight. The "mu" field currently stored
+    # is 1.0 with units of kg, which is wrong.
+    Y = data['gas','yhe'].d
+    X = 1-Y
+    Z = data['gas','metal_fraction'].d
+
+    return 1 / (X + 0.25*Y + 0.0625*Z) # metals weighted by 1/16 assuming oxygen-dominated
+
+def _temperature(field, data):
+    gamma = 5/3
+    return data['gas','MMW'] * mp * data['gas','internal_energy'].to('cm**2/s**2') / kB * (gamma-1)
+
+def _number_density(field, data):
+    return data['gas','density'].to('g/cm**3') / (data['gas','MMW']*mp)
+
+def _metallicity(field, data):
+    # metallicity in solar units
+    return data["gas","metal_fraction"] / solar_metallicity
+
+def _emission_measure(field, data):
+    # emission_measure = ne**2 * particle_volume
+
+    # assume ne=nH -- safe assumption for cluster scale objects where gas is fully ionized
+    ne = (1-data['gas','yhe'].d)*data['gas','number_density']
+    return ne**2 * (data['gas','particle_mass']/data['gas','density']).to('cm**3')
+    
