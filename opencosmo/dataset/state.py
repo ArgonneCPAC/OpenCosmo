f1b9811c5c52b41ee16a632a846a959b30504ebc
Patrick Wells <patrick.wells.95@gmail.com>
2025-06-17 09:07:48 -0500

    Continue to update derives + documentation

--- opencosmo/dataset/state.py
+++ opencosmo/dataset/state.py
@@ -16,8 +16,8 @@ if TYPE_CHECKING:
 
 class DatasetState:
     """
-    Holds any mutable state required by the dataset. Anything that remains unchanged
-    will be held by the datases itself
+    Holds mutable state required by the dataset. Cleans up the dataset to mostly focus
+    on very high-level operations. Not a user facing class.
     """
 
     def __init__(
@@ -59,13 +59,19 @@ class DatasetState:
         return list(self.__builders.keys()) + list(self.__derived.keys())
 
     def get_data(self, handler: "DatasetHandler"):
+        """
+        Get the data for a given handler.
+        """
         data = handler.get_data(builders=self.__builders, index=self.__index)
-        data = self.build_derived_columns(data)
+        data = self.__build_derived_columns(data)
         if self.__hidden:
             data.remove_columns(self.__hidden)
         return data
 
     def with_index(self, index: DataIndex):
+        """
+        Return the same dataset state with a new index
+        """
         return DatasetState(
             self.__base_unit_transformations,
             self.__builders,
@@ -77,6 +83,10 @@ class DatasetState:
         )
 
     def with_derived_columns(self, **new_columns: DerivedColumn):
+        """
+        Add a set of derived columns to the dataset. A derived column is a column that
+        has been created based on the values in another column.
+        """
         column_names = set(self.builders.keys()) | set(self.__derived.keys())
         for name, new_col in new_columns.items():
             if name in column_names:
@@ -99,24 +109,19 @@ class DatasetState:
             new_derived,
         )
 
-    def build_derived_columns(self, data: table.Table) -> table.Table:
+    def __build_derived_columns(self, data: table.Table) -> table.Table:
+        """
+        Build any derived columns that are present in this dataset
+        """
         for colname, column in self.__derived.items():
             new_column = column.evaluate(data)
             data[colname] = new_column
         return data
 
-    def with_builders(self, builders: dict[str, ColumnBuilder]):
-        return DatasetState(
-            self.__base_unit_transformations,
-            builders,
-            self.__index,
-            self.__convention,
-            self.__region,
-            self.__hidden,
-            self.__derived,
-        )
-
     def with_region(self, region: Region):
+        """
+        Return the same dataset but with a different region
+        """
         return DatasetState(
             self.__base_unit_transformations,
             self.__builders,
@@ -128,6 +133,15 @@ class DatasetState:
         )
 
     def select(self, columns: str | Iterable[str]):
+        """
+        Select a subset of columns from the dataset. It is possible for a user to select
+        a derived column in the dataset, but not the columns it is derived from.
+        This class tracks any columns which are required to materialize the dataset but
+        are not in the final selection in self.__hidden. When the dataset is
+        materialized, the columns in self.__hidden are removed before the data is
+        returned to the user.
+
+        """
         if isinstance(columns, str):
             columns = [columns]
 
@@ -144,20 +158,29 @@ class DatasetState:
 
         required_derived = known_derived.intersection(columns)
         required_builders = known_builders.intersection(columns)
+        additional_derived = required_derived
 
-        additional_columns: set[str] = reduce(
-            lambda s, derived: s.union(self.__derived[derived].requires()),
-            required_derived,
-            set(),
-        )
-        required_derived |= additional_columns.intersection(known_derived)
-        required_builders |= additional_columns.intersection(known_builders)
+        while additional_derived:
+            # Follow any chains of derived columns until we reach columns that are
+            # actually in the raw data.
+            required_derived |= additional_derived
+            additional_columns: set[str] = reduce(
+                lambda s, derived: s.union(self.__derived[derived].requires()),
+                additional_derived,
+                set(),
+            )
+            required_builders |= additional_columns.intersection(known_builders)
+            additional_derived = additional_columns.intersection(known_derived)
+
         all_required = required_derived | required_builders
 
-        new_derived = {d: self.__derived[d] for d in required_derived}
+        # Derived columns have to be instantiated in the order they are created in order
+        # to ensure chains of derived columns work correctly
+        new_derived = {k: v for k, v in self.__derived.items() if k in required_derived}
+        # Builders can be performed in any order
         new_builders = {b: self.__builders[b] for b in required_builders}
 
-        new_hidden = self.__hidden | (all_required - columns)
+        new_hidden = all_required - columns
 
         return DatasetState(
             self.__base_unit_transformations,
@@ -170,10 +193,16 @@ class DatasetState:
         )
 
     def take(self, n: int, at: str):
+        """
+        Take rows from the dataset.
+        """
         new_index = self.__index.take(n, at)
         return self.with_index(new_index)
 
     def take_range(self, start: int, end: int):
+        """
+        Take a range of rows form the dataset.
+        """
         if start < 0 or end < 0:
             raise ValueError("start and end must be positive.")
         if end < start:
@@ -188,6 +217,9 @@ class DatasetState:
         return self.with_index(new_index)
 
     def with_units(self, convention: str, cosmology: Cosmology, redshift: float):
+        """
+        Change the unit convention
+        """
         new_transformations = u.get_unit_transition_transformations(
             convention, self.__base_unit_transformations, cosmology, redshift
         )
