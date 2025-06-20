ec0b2322677e63c0db661100c92eddbd117b26b6
Patrick Wells <patrick.wells.95@gmail.com>
2025-06-20 16:08:39 -0500

    Derived columns working in collections

--- test/test_derive.py
+++ test/test_derive.py
@@ -17,7 +17,7 @@ def particles_path(snapshot_path):
 def test_derive_multiply(properties_path):
     ds = oc.open(properties_path)
     derived = oc.col("fof_halo_mass") * oc.col("fof_halo_com_vx")
-    ds = ds.add_columns(fof_halo_px=derived)
+    ds = ds.with_new_columns(fof_halo_px=derived)
     data = ds.data
     assert "fof_halo_px" in data.columns
     assert (
@@ -33,7 +33,7 @@ def test_derive_multiply(properties_path):
 def test_derive_write(properties_path, tmp_path):
     ds = oc.open(properties_path)
     derived = oc.col("fof_halo_mass") * oc.col("fof_halo_com_vx")
-    ds = ds.add_columns(fof_halo_px=derived)
+    ds = ds.with_new_columns(fof_halo_px=derived)
     data = ds.data
     oc.write(tmp_path / "test.hdf5", ds)
     written_data = oc.open(tmp_path / "test.hdf5").data
@@ -45,7 +45,7 @@ def test_derive_write(properties_path, tmp_path):
 def test_derive_divide(properties_path):
     ds = oc.open(properties_path)
     derived = oc.col("fof_halo_mass") / oc.col("fof_halo_com_vx")
-    ds = ds.add_columns(fof_halo_px=derived)
+    ds = ds.with_new_columns(fof_halo_px=derived)
     data = ds.data
     assert "fof_halo_px" in data.columns
     assert (
@@ -63,7 +63,7 @@ def test_derive_chain(properties_path):
     derived = oc.col("fof_halo_mass") * (
         oc.col("fof_halo_com_vx") * oc.col("fof_halo_com_vy")
     )
-    ds = ds.add_columns(fof_halo_p_sqr=derived)
+    ds = ds.with_new_columns(fof_halo_p_sqr=derived)
     data = ds.data
     assert "fof_halo_p_sqr" in data.columns
     assert (
@@ -90,7 +90,7 @@ def test_scalars(properties_path):
     derived2 = 3.0 * oc.col("fof_halo_mass")
     derived3 = 1 / oc.col("fof_halo_mass")
     derived4 = oc.col("fof_halo_mass") / 2.0
-    ds = ds.add_columns(
+    ds = ds.with_new_columns(
         derived1=derived1, derived2=derived2, derived3=derived3, derived4=derived4
     )
 
@@ -109,7 +109,7 @@ def test_power(properties_path):
         + oc.col("fof_halo_com_vz") ** 2
     ) ** 0.5
     ke = 0.5 * oc.col("fof_halo_mass") * total_speed**2
-    ds = ds.add_columns(ke=ke)
+    ds = ds.with_new_columns(ke=ke)
     data = ds.data
     assert (
         data["ke"].unit
@@ -132,7 +132,7 @@ def test_power(properties_path):
 def test_derive_unit_change(properties_path):
     ds = oc.open(properties_path)
     derived = oc.col("fof_halo_mass") * oc.col("fof_halo_com_vx")
-    ds = ds.add_columns(fof_halo_px=derived)
+    ds = ds.with_new_columns(fof_halo_px=derived)
     comoving_data = ds.data
     comoving_unit = comoving_data["fof_halo_px"].unit
     ds = ds.with_units("scalefree")
@@ -153,7 +153,7 @@ def test_derive_unit_change(properties_path):
 def test_derive_mask(properties_path):
     ds = oc.open(properties_path)
     derived = oc.col("fof_halo_mass") * oc.col("fof_halo_com_vx")
-    ds = ds.add_columns(fof_halo_px=derived)
+    ds = ds.with_new_columns(fof_halo_px=derived)
     comoving_data = ds.data["fof_halo_px"]
     val = 0.5 * (comoving_data.max() - comoving_data.min()) + comoving_data.min()
     ds_masked = ds.filter(oc.col("fof_halo_px") > val)
@@ -163,9 +163,9 @@ def test_derive_mask(properties_path):
 def test_derive_children(properties_path):
     ds = oc.open(properties_path)
     derived = oc.col("fof_halo_mass") * oc.col("fof_halo_com_vx")
-    ds = ds.add_columns(fof_halo_px=derived)
+    ds = ds.with_new_columns(fof_halo_px=derived)
     derived2 = 0.5 * oc.col("fof_halo_px") * oc.col("fof_halo_com_vx")
-    ds = ds.add_columns(derived2=derived2)
+    ds = ds.with_new_columns(derived2=derived2)
     data = ds.data
     assert np.all(
         np.isclose(
@@ -177,9 +177,9 @@ def test_derive_children(properties_path):
 def test_derive_children_select(properties_path):
     ds = oc.open(properties_path)
     derived = oc.col("fof_halo_mass") * oc.col("fof_halo_com_vx")
-    ds = ds.add_columns(fof_halo_px=derived)
+    ds = ds.with_new_columns(fof_halo_px=derived)
     derived2 = 0.5 * oc.col("fof_halo_px") * oc.col("fof_halo_com_vx")
-    ds = ds.add_columns(derived2=derived2)
+    ds = ds.with_new_columns(derived2=derived2)
     derived_data = ds.data["derived2"]
 
     to_select = ["fof_halo_com_vy", "derived2"]
@@ -191,7 +191,7 @@ def test_derive_children_select(properties_path):
 
 def test_derive_structure_collection(properties_path, particles_path):
     ds = oc.structure.open_linked_files(properties_path, particles_path)
-    ds["dm_particles"].add_columns(gpe=oc.col("mass") * oc.col("phi"))
+    ds = ds.with_new_columns("dm_particles", gpe=oc.col("mass") * oc.col("phi"))
     ds = ds.filter(oc.col("fof_halo_mass") > 1e14)
     ds = ds.take(1, at="random")
     for properties, particles in ds.objects(["dm_particles"]):
