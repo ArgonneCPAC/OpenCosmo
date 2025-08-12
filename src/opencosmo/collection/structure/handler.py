549e34946add9b7ded2d97fa3e825dbbf0b6c154
Patrick Wells <patrick.wells.95@gmail.com>
2025-08-12 09:57:10 -0500

    Partial rever

--- src/opencosmo/collection/structure/handler.py
+++ src/opencosmo/collection/structure/handler.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 import h5py
 import numpy as np
 
-from opencosmo.index import DataIndex
+from opencosmo.index import ChunkedIndex, DataIndex, SimpleIndex
 from opencosmo.io import schemas as ios
 
 if TYPE_CHECKING:
@@ -35,17 +35,22 @@ class LinkedDatasetHandler:
             rows = index.get_data(self.link)
             return rows != -1
 
-    def make_indices(self, index: DataIndex) -> tuple[np.ndarray, Optional[np.ndarray]]:
+    def make_index(self, index: DataIndex) -> DataIndex:
         if isinstance(self.link, tuple):
             start = index.get_data(self.link[0])
             size = index.get_data(self.link[1])
             valid_rows = size > 0
-            starts = start[valid_rows]
-            sizes = size[valid_rows]
-            return starts, sizes
+            start = start[valid_rows]
+            size = size[valid_rows]
+            if not start.size:
+                return SimpleIndex(np.array([], dtype=int))
+            else:
+                return ChunkedIndex(start, size)
         else:
             indices_into_data = index.get_data(self.link)
-            return indices_into_data[indices_into_data >= 0], None
+            indices_into_data = indices_into_data[indices_into_data >= 0]
+
+            return SimpleIndex(indices_into_data)
 
     def make_schema(self, name: str, index: DataIndex) -> ios.LinkSchema:
         if isinstance(self.link, h5py.Dataset):
