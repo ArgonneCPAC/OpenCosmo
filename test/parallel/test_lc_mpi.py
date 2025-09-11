a909b976216cea19d35f3f662ce355a0fdef6c00
Patrick Wells <patrick.wells.95@gmail.com>
2025-09-11 14:29:14 -0500

    debug

--- test/parallel/test_lc_mpi.py
+++ test/parallel/test_lc_mpi.py
@@ -147,6 +147,7 @@ def test_write_some_missing(core_path_487, core_path_475, tmp_path):
     if comm.Get_rank() == 0:
         ds = ds.with_redshift_range(0, 0.02)
         assert len(ds.keys()) == 1
+    assert False
     original_data = ds.select("early_index").get_data()
     original_data_length = comm.allgather(len(original_data))
 
