524efbdf3675c30d751145c9e7987221a8739860
Patrick Wells <patrick.wells.95@gmail.com>
2025-09-11 14:52:36 -0500

    debug

--- src/opencosmo/io/writers.py
+++ src/opencosmo/io/writers.py
@@ -78,8 +78,6 @@ class CollectionWriter:
         self.header = header
 
     def write(self, file: h5py.File | h5py.Group):
-        print(self.children)
-        assert False
         if len(self.children) == 1:
             next(iter(self.children.values())).write(file)
             return
@@ -89,6 +87,7 @@ class CollectionWriter:
         for name in child_names:
             self.children[name].write(file[name])
 
+        assert False
         if self.header is not None:
             self.header.write(file)
 
