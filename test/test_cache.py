add36ddcdd60c7d9155ac59b9295e74257ff271a
Patrick Wells <patrick.wells.95@gmail.com>
2025-10-23 09:16:26 -0500

    Cache passthrough working

--- test/test_cache.py
+++ test/test_cache.py
@@ -52,14 +52,42 @@ def test_cache_passthrough_delete():
     indices2 = np.sort(np.random.choice(10_000, 1000, replace=False))
     index2 = SimpleIndex(indices2)
     cache2 = cache.take(index2)
+    _ = cache2.get_columns("abcdefg")
 
     indices3 = np.sort(np.random.choice(1000, 100, replace=False))
     index3 = SimpleIndex(indices3)
     cache3 = cache2.take(index3)
 
+    assert set(cache3._ColumnCache__columns.keys()) == set()
     del cache2
-
+    assert set(cache3._ColumnCache__columns.keys()) == set("abcdefg")
     cached_data = cache3.get_columns("abcdefg")
+
     for colname, column in cached_data.items():
         assert np.all(column == data[colname][indices2[indices3]])
-    assert len(cache3._ColumnCache__columns.keys()) == 0
+
+
+def test_cache_passthrough_twice_delete():
+    data = {}
+    for name in "abcdefg":
+        data[name] = np.random.randint(0, 1000, 10_000)
+    cache = ColumnCache.empty()
+    cache.add_data(data)
+
+    indices2 = np.sort(np.random.choice(10_000, 1000, replace=False))
+    index2 = SimpleIndex(indices2)
+    cache2 = cache.take(index2)
+
+    indices3 = np.sort(np.random.choice(1000, 100, replace=False))
+    index3 = SimpleIndex(indices3)
+    cache3 = cache2.take(index3)
+
+    assert set(cache2._ColumnCache__columns.keys()) == set()
+    del cache
+    assert set(cache2._ColumnCache__columns.keys()) == set("abcdefg")
+
+    cached_data = cache3.get_columns("abcdefg")
+
+    for colname, column in cached_data.items():
+        assert np.all(column == data[colname][indices2[indices3]])
+    assert set(cache3._ColumnCache__columns.keys()) == set("abcdefg")
