Fix a bug that could cause units to be stripped after being cached

src/opencosmo/column/cache.py
@@ -121,6 +121,7 @@ class ColumnCache:
                 colname: self.__derived_index.get_data(coldata)
                 for colname, coldata in cached_data.items()
             }
+
         self.__cached_data |= cached_data
 
     def __push_up(self, data: dict[str, np.ndarray]):
@@ -147,7 +148,6 @@ class ColumnCache:
         }
         if not cached_data:
             return
-
         for child_ in self.__children:
             if (child := child_()) is None:
                 continue
@@ -195,6 +195,7 @@ class ColumnCache:
             and (p := self.__parent()) is not None
         ):
             p.__push_up(data)
+
         self.__cached_data = self.__cached_data | data
 
     def with_data(

test/parallel/test_mpi.py
@@ -407,8 +407,8 @@ def test_box_query_chain(input_path):
     reg2 = oc.make_box(p1, p2)
     ds = ds.bound(reg1)
     ds = ds.bound(reg2)
+    data = ds.get_data()
 
-    data = ds.data
     for i, dim in enumerate(["x", "y", "z"]):
         colname = f"fof_halo_center_{dim}"
         col = data[colname]
@@ -418,10 +418,8 @@ def test_box_query_chain(input_path):
             original_data[colname].value > min_
         )
         original_data = original_data[mask]
-        min = col.min()
-        max = col.max()
-        parallel_assert(min.value >= min_)
-        parallel_assert(max.value <= max_)
+        parallel_assert(col.min().value >= min_)
+        parallel_assert(col.max().value <= max_)
 
     parallel_assert(set(original_data["fof_halo_tag"]) == set(data["fof_halo_tag"]))
 
@@ -505,7 +503,7 @@ def test_evaluate(input_path):
     assert np.all(data["fof_px"] == data["fof_halo_mass"] * data["fof_halo_com_vx"])
 
 
-@pytest.mark.timeout(0.25, method="thread")
+@pytest.mark.timeout(20, method="thread")
 @pytest.mark.parallel(nprocs=4)
 def test_evaluate_write(input_path, tmp_path):
     comm = mpi4py.MPI.COMM_WORLD