3faf7fa9aadd23b25f450cf1bd1fe1b098f1d39e
Patrick Wells <patrick.wells.95@gmail.com>
2025-11-06 11:57:04 -0600

    Update some magnitude tests

--- test/test_diffsky.py
+++ test/test_diffsky.py
@@ -3,6 +3,7 @@ import numpy as np
 import pytest
 
 import opencosmo as oc
+from opencosmo.column import add_mag_cols
 
 
 @pytest.fixture
@@ -84,7 +85,7 @@ def test_add_logarithmic_units(core_path_487):
 def test_add_two_non_logarithmic_units(core_path_487):
     ds = oc.open(core_path_487, synth_cores=True)
     log_sfr = oc.col("dec") + oc.col("x_nfw")
-    with pytest.raises(oc.dataset.column.UnitsError):
+    with pytest.raises(oc.units.UnitsError):
         ds = ds.with_new_columns(log_sfr=log_sfr)
 
 
@@ -98,20 +99,37 @@ def test_mag_units(core_path_475, core_path_487):
 
 def test_add_mag_units(core_path_475, core_path_487):
     ds = oc.open(core_path_487, core_path_475)
-    mag_columns = filter(lambda c: "lsst_" in c, ds.columns)
-    total_mag = add_mag_cols(*list(mag_columns))
+    mag_columns = list(filter(lambda c: "lsst_" in c, ds.columns))
+    total_mag = add_mag_cols(*mag_columns)
     ds = ds.with_new_columns(lsst_total=total_mag)
-    mag_columns = filter(lambda c: "lsst_" in c, ds.columns)
-    assert False
+
+    mag_columns += ["lsst_total"]
+
+    total_mag = np.zeros(len(ds), dtype=float)
+    data = ds.select(mag_columns).get_data("numpy")
+    for column in mag_columns:
+        if column == "lsst_total":
+            continue
+        total_mag += 10 ** (-0.4 * data[column])
+
+    total_mag = -2.5 * np.log10(total_mag)
+
+    assert np.all(data["lsst_total"] == total_mag)
 
 
 def test_add_mag_units_unitless(core_path_475, core_path_487):
-    from opencosmo.column import add_mag_cols
-
     ds = oc.open(core_path_487, core_path_475).with_units("unitless")
-    mag_columns = filter(lambda c: "lsst_" in c, ds.columns)
-    total_mag = add_mag_cols(*list(mag_columns))
+    mag_columns = list(filter(lambda c: "lsst_" in c, ds.columns))
+    total_mag = add_mag_cols(*mag_columns)
     ds = ds.with_new_columns(lsst_total=total_mag)
-    mag_columns = filter(lambda c: "lsst_" in c, ds.columns)
-    print(ds.select(mag_columns).take(10, at="start").get_data())
-    assert False
+    mag_columns += ["lsst_total"]
+
+    data = ds.select(mag_columns).get_data("numpy")
+    total_mag = np.zeros(len(ds), dtype=float)
+    for column in mag_columns:
+        if column == "lsst_total":
+            continue
+        total_mag += 10 ** (-0.4 * data[column])
+
+    total_mag = -2.5 * np.log10(total_mag)
+    assert np.all(data["lsst_total"] == total_mag)
