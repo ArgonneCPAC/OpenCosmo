7bcb631c6dce8d56998a611c8c58017582f45d3d
Patrick Wells <patrick.wells.95@gmail.com>
2025-11-25 08:03:54 -0600

    Basic dataset building logic

---
+++ src/opencosmo/dataset/build.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import uuid
+from typing import TYPE_CHECKING, Optional
+
+import h5py
+import numpy as np
+
+from opencosmo.dataset import Dataset
+from opencosmo.dataset.state import DatasetState
+
+if TYPE_CHECKING:
+    from opencosmo.header import OpenCosmoHeader
+    from opencosmo.spatial import Region
+
+GroupedColumnData = dict[str, dict[str, np.ndarray]]
+
+
+def build_dataset_from_data(
+    data: GroupedColumnData | h5py.Group,
+    header: OpenCosmoHeader,
+    region: Region,
+    spatial_index_data: Optional[GroupedColumnData | h5py.Group] = None,
+) -> Dataset:
+    data_keys = set(data.keys())
+    metadata_group = None
+    if not data_keys or len(data_keys) > 2 or "data" not in data_keys:
+        raise ValueError(
+            "Data must have at least one `data` group and at most one metadata group"
+        )
+
+    if isinstance(data, dict):
+        data = make_in_memory_h5_file_from_data(data)
+    if isinstance(spatial_index_data, dict):
+        spatial_index_data = make_in_memory_h5_file_from_data(spatial_index_data)
+    if len(data_keys) == 2:
+        data_keys.remove("data")
+        metadata_key = data_keys.pop()
+        metadata_group = data[metadata_key]
+
+    new_state = DatasetState.from_group(
+        data["data"],
+        header,
+        header.file.unit_convention,
+        region,
+        metadata_group=metadata_group,
+    )
+    return Dataset(header, new_state)
+
+
+def make_in_memory_h5_file_from_data(data: GroupedColumnData) -> h5py.File:
+    name = uuid.uuid1()
+    file = h5py.File(f"{name}.hdf5", "w", driver="core", backing_store=False)
+    for group_name, columns in data.items():
+        lengths = set(len(c) for c in columns.values())
+        if len(lengths) > 1:
+            raise ValueError("Columns within a single group must be the same length!")
+        group = file.require_group(group_name)
+        for column_name, column_data in columns.items():
+            group.create_dataset(column_name, column_data)
+    file.mode = "r"
+    return file
