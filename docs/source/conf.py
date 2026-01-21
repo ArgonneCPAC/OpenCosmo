f872fa70fec3e8b3918c1264c80a959873b79df3
Patrick Wells <patrick.wells.95@gmail.com>
2026-01-21 07:46:50 -0600

    Add filter fix, bump everything to 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "OpenCosmo"
 copyright = "2025, OpenCosmo Team"
 author = "OpenCosmo Team"
-release = "1.0.0rc3"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
