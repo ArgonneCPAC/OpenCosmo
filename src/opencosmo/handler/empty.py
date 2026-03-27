aff092fb2b1aa107c1c2efc1ee61792cc969b258
Patrick Wells <patrick.wells.95@gmail.com>
2026-03-26 19:31:56 -0500

    Add new handler directory

---
+++ src/opencosmo/handler/empty.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Iterable, Optional, Self
+
+from opencosmo.index import empty
+from opencosmo.io.schema import FileEntry, make_schema
+
+if TYPE_CHECKING:
+    import numpy as np
+
+    from opencosmo.index import DataIndex
+
+
+class EmptyHandler:
+    def get_data(self, *args):
+        return {}
+
+    def get_metadata(self, columns: Iterable[str]) -> Optional[dict[str, np.ndarray]]:
+        return None
+
+    def take(self, other: DataIndex, sorted: Optional[np.ndarray] = None) -> Self:
+        return self
+
+    def __len__(self) -> int:
+        return 0
+
+    def make_schema(self, *args, **kwargs):
+        data_schema = make_schema("data", FileEntry.COLUMNS)
+        metadata_schema = make_schema("metadata_data", FileEntry.COLUMNS)
+        return data_schema, metadata_schema
+
+    @property
+    def columns(self) -> Iterable[str]:
+        return set()
+
+    @property
+    def load_conditions(self):
+        return None
+
+    @property
+    def metadata_columns(self) -> Iterable[str]:
+        return set()
+
+    @property
+    def descriptions(self):
+        return {}
+
+    @property
+    def index(self) -> DataIndex:
+        return empty()
---
+++ src/opencosmo/handler/protocols.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Iterable, Optional, Protocol, Self
+
+if TYPE_CHECKING:
+    import numpy as np
+
+    from opencosmo.header import OpenCosmoHeader
+    from opencosmo.index import DataIndex
+    from opencosmo.io.schema import Schema
+
+
+class DataHandler(Protocol):
+    def get_data(self, columns: Iterable[str]) -> dict[str, np.ndarray]:
+        """ """
+
+    def get_metadata(
+        self, columns: Iterable[str]
+    ) -> Optional[dict[str, np.ndarray]]: ...
+
+    def take(self, other: DataIndex, sorted: Optional[np.ndarray] = None) -> Self: ...
+
+    def make_schema(
+        self, columns: Iterable[str], header: Optional[OpenCosmoHeader] = None
+    ) -> (Schema, Schema): ...
+
+    @property
+    def columns(self) -> Iterable[str]: ...
+    @property
+    def metadata_columns(self) -> Iterable[str]: ...
+    @property
+    def load_conditions(self) -> Optional[dict]: ...
+
+    @property
+    def index(self) -> DataIndex: ...
+
+
+class DataCache(DataHandler):
+    def add_data(
+        self,
+        data: dict[str, np.ndarray],
+        descriptions: dict[str, str],
+        push_up: bool = True,
+    ): ...
+
+    def __len__(self) -> int: ...
+
+    def register_column_group(self, state_id: int, columns: set[str]) -> None: ...
+    def deregister_column_group(self, state_id: int) -> None: ...
