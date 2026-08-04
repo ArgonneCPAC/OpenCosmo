9e8d12007cdcb9b3b408ebb8cadd42cb9c06997a
Patrick Wells <patrick.wells.95@gmail.com>
2026-08-04 12:18:47 -0500

    Turn off caching for metadata columns

--- python/opencosmo/column/column.py
+++ python/opencosmo/column/column.py
@@ -654,12 +654,21 @@ class ConstructedColumn(Protocol):
 
 
 class RawColumn:
-    def __init__(self, name, description, alias=None, _dep_uuid=None, _uuid=None):
+    def __init__(
+        self,
+        name,
+        description,
+        alias=None,
+        _dep_uuid=None,
+        _uuid=None,
+        no_cache: bool = False,
+    ):
         self.__name = name
         self.__description = description
         self.__alias = alias
         self.__uuid = _uuid if _uuid is not None else uuid4()
         self.__dep_uuid: UUID | None = _dep_uuid
+        self.__no_cache = no_cache
 
     @property
     def uuid(self) -> UUID:
@@ -703,7 +712,7 @@ class RawColumn:
 
     @property
     def no_cache(self):
-        return False
+        return self.no_cache
 
     @property
     def alias(self) -> str | None:
